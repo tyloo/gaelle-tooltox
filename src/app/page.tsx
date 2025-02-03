235612f21d9ab1f0df624412eab420b0a3f1e1e8
tyloo <jbonva@gmail.com>
2025-02-03 17:22:54 +0100

    chore: Added Suspense on homepage
    Signed-off-by: tyloo <jbonva@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { DogCard } from '@/components/dog-card'
 import { JokeCard } from '@/components/joke-card'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center gap-4 h-[calc(100vh_-_64px)] p-2'>
-      <JokeCard />
-      <DogCard />
+      <Suspense fallback={'Loading...'}>
+        <JokeCard />
+        <DogCard />
+      </Suspense>
     </div>
   )
 }
